ad3574885573e388e2f8fe1accce4244fc2e9f0d
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-02-10 13:47:34 +0100

    Update socket to latest

--- src/app.js
+++ src/app.js
@@ -4,14 +4,25 @@ import { handleC2C } from '.';
 
 var app = express();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+
+const corsOption = {
+  credentials: true,
+  origin: (origin, callback) => {
+    // Allow ALL origins pls
+    return callback(null, true);
+  },
+};
+
+var io = require('socket.io')(http, {
+  cors: corsOption,
+});
 
 const port = process.env.PORT || 4000;
 
-http.listen(port, () => {
-  console.log(`listening on *:${port}`);
-});
-
 io.on('connection', (socket) => {
   handleC2C(socket);
+});
+
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
