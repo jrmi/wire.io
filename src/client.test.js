816a575a0f029dd4e7f6dbb7df97d0660678c255
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-05-21 17:42:14 +0200

    Add room prefix to all events

--- src/client.test.js
+++ src/client.test.js
@@ -68,7 +68,7 @@ describe('Client', () => {
   it('should receive incoming user event', async (done) => {
     const room1 = await join({ socket: socket1, room: 'test' });
 
-    room1.subscribe('userEnter', (params) => {
+    room1.subscribe('test.userEnter', (params) => {
       expect(params).toBeDefined();
       done();
     });
@@ -78,7 +78,7 @@ describe('Client', () => {
 
   it('should receive user leave event', async (done) => {
     const room1 = await join({ socket: socket1, room: 'test' });
-    room1.subscribe('userLeave', (params) => {
+    room1.subscribe('test.userLeave', (params) => {
       expect(params).toBeDefined();
       done();
     });
