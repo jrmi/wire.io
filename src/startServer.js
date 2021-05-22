efd1440a7706d32af23669ce4dcaad11c8afecf2
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-05-22 11:48:37 +0200

    Add server command

---
+++ src/startServer.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { createServer } from 'http';
+import io from 'socket.io';
+import cors from 'cors';
+
+import { handleC2C } from '.';
+
+const startServer = ({ socketIOConfig = {}, port = 4000 }) => {
+  var app = express();
+  var http = createServer(app);
+
+  const corsOption = {
+    credentials: true,
+    origin: (origin, callback) => {
+      // Allow ALL origins pls
+      return callback(null, true);
+    },
+  };
+
+  app.use(cors(corsOption));
+
+  const ioServer = io(http, {
+    ...socketIOConfig,
+  });
+
+  ioServer.on('connection', (socket) => {
+    handleC2C(socket, { log: (msg) => console.log(msg) });
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Ok');
+  });
+
+  http.listen(port, () => {
+    console.log(`listening on *:${port}`);
+  });
+};
+
+export default startServer;
